f474976915b9c8838cb799bde80a59612fa8ebf4
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-22 11:27:55 +0100

    Import and split Data (data oading and Preprocessing)

--- Image_segmentation_Unet.py
+++ Image_segmentation_Unet.py
@@ -8,3 +8,16 @@ from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Conv2DTranspose
 from tensorflow.keras.layers import concatenate
 
+# Import and split Data (data oading and Preprocessing)
+import os
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import imageio
+import matplotlib.pyplot as plt
+
+path = ''
+image_path = os.path.join(path, './data/CameraRGB/')
+mask_path = os.path.join(path, './data/CameraMask/')
+image_list = os.listdir(image_path)
+mask_list = os.listdir(mask_path)
+image_list = [image_path+i for i in image_list]
+mask_list = [mask_path+i for i in mask_list]
