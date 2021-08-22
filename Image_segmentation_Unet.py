b9407117354dff1674ea79262e8f1252400848d6
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-22 12:16:43 +0100

    Inplement a Funct. defining a Conv Layer with optional dropout and Maxpooling layers as hyperparames

--- Image_segmentation_Unet.py
+++ Image_segmentation_Unet.py
@@ -21,3 +21,91 @@ image_list = os.listdir(image_path)
 mask_list = os.listdir(mask_path)
 image_list = [image_path+i for i in image_list]
 mask_list = [mask_path+i for i in mask_list]
+
+# Data Viz : CHeck out soe of teh unmasked images
+N = 4
+img = imageio.imread(image_list[N])
+mask = imageio.imread(mask_list[N])
+#mask = np.array([max(mask[i, j]) for i in range(mask.shape[0]) for j in range(mask.shape[1])]).reshape(img.shape[0], img.shape[1])
+fig, arr = plt.subplots(1, 2, figsize=(14, 10))
+arr[0].imshow(img)
+arr[0].set_title('Image')
+arr[1].imshow(mask[:, :, 0])
+arr[1].set_title('Segmentation')
+
+#  Split the Dataset into Unmasked and Masked Images
+image_list_ds = tf.data.Dataset.list_files(image_list, shuffle=False)
+mask_list_ds = tf.data.Dataset.list_files(mask_list, shuffle=False)
+
+for path in zip(image_list_ds.take(3), mask_list_ds.take(3)):
+    print(path)
+    image_filenames = tf.constant(image_list)
+    masks_filenames = tf.constant(mask_list)
+
+    dataset = tf.data.Dataset.from_tensor_slices((image_filenames, masks_filenames))
+
+    for image, mask in dataset.take(1):
+        print(image)
+        print(mask)
+
+# Preprocess Data : Data Preprocessing
+
+def process_path(image_path, mask_path):
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_png(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    mask = tf.io.read_file(mask_path)
+    mask = tf.image.decode_png(mask, channels=3)
+    mask = tf.math.reduce_max(mask, axis=-1, keepdims=True)
+    return img, mask
+
+def preprocess(image, mask):
+    input_image = tf.image.resize(image, (96, 128), method='nearest')
+    input_mask = tf.image.resize(mask, (96, 128), method='nearest')
+
+    return input_image, input_mask
+
+image_ds = dataset.map(process_path)
+processed_image_ds = image_ds.map(preprocess)
+
+# Inplement a Funct. defining a Conv Layer with optional dropout and Maxpooling layers as hyperparames
+def conv_block(inputs=None, n_filters=32, dropout_prob=0, max_pooling=True):
+    """
+    Convolutional downsampling block
+
+    Arguments:
+        inputs -- Input tensor
+        n_filters -- Number of filters for the convolutional layers
+        dropout_prob -- Dropout probability
+        max_pooling -- Use MaxPooling2D to reduce the spatial dimensions of the output volume
+    Returns:
+        next_layer, skip_connection --  Next layer and skip connection outputs
+    """
+    conv = Conv2D(n_filters,  # Number of filters
+                  3,  # Kernel size
+                  activation='relu',
+                  padding='same',
+                  kernel_initializer='he_normal')(inputs)
+    conv = Conv2D(n_filters,  # Number of filters
+                  3,  # Kernel size
+                  activation='relu',
+                  padding='same',
+                  kernel_initializer='he_normal')(conv)
+
+    # if dropout_prob > 0 add a dropout layer, with the variable dropout_prob as parameter
+    if dropout_prob > 0:
+        conv = Dropout(dropout_prob)(conv)
+
+    # if max_pooling is True add a MaxPooling2D with 2x2 pool_size
+    if max_pooling:
+
+        next_layer = MaxPooling2D(pool_size=(2, 2))(conv)
+
+
+    else:
+        next_layer = conv
+
+    skip_connection = conv
+
+    return next_layer, skip_connection
