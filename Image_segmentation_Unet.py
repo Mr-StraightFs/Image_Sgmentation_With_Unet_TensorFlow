9a2badb4497a97c6def86d6a85d3886343e54740
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-22 13:14:47 +0100

    Train teh Model , Evaluate results and Show Predictions

--- Image_segmentation_Unet.py
+++ Image_segmentation_Unet.py
@@ -145,3 +145,127 @@ def upsampling_block(expansive_input, contractive_input, n_filters=32):
                   kernel_initializer='he_normal')(conv)
 
     return conv
+
+# Implement the UNet Model :
+def unet_model(input_size=(96, 128, 3), n_filters=32, n_classes=23):
+    """
+    Unet model
+
+    Arguments:
+        input_size -- Input shape
+        n_filters -- Number of filters for the convolutional layers
+        n_classes -- Number of output classes
+    Returns:
+        model -- tf.keras.Model
+    """
+    inputs = Input(input_size)
+    # Contracting Path (encoding)
+    # Add a conv_block with the inputs of the unet_ model and n_filters
+
+    cblock1 = conv_block(inputs, n_filters)
+    # Chain the first element of the output of each block to be the input of the next conv_block.
+    # Double the number of filters at each new step
+    cblock2 = conv_block(cblock1[0], n_filters * 2)
+    cblock3 = conv_block(cblock2[0], n_filters * 2 * 2)
+    cblock4 = conv_block(cblock3[0], n_filters * 2 * 2 * 2,dropout_prob=0.3)  # Include a dropout_prob of 0.3 for this layer
+    # Include a dropout_prob of 0.3 for this layer, and avoid the max_pooling layer
+    cblock5 = conv_block(cblock4[0], n_filters * 2 * 2 * 2 * 2, dropout_prob=0.3, max_pooling=0)
+
+    # Expanding Path (decoding)
+    # Add the first upsampling_block.
+    # Use the cblock5[0] as expansive_input and cblock4[1] as contractive_input and n_filters * 8
+
+    ublock6 = upsampling_block(cblock5[0], cblock4[1], n_filters * 8)
+    # Chain the output of the previous block as expansive_input and the corresponding contractive block output.
+    # At each step, we'll half the number of filters of the previous block
+    ublock7 = upsampling_block(ublock6, cblock3[1], n_filters * 4)
+    ublock8 = upsampling_block(ublock7, cblock2[1], n_filters * 2)
+    ublock9 = upsampling_block(ublock8, cblock1[1], n_filters)
+
+    conv9 = Conv2D(n_filters,
+                   3,
+                   activation='relu',
+                   padding='same',
+                   kernel_initializer='he_normal')(ublock9)
+
+    # Add a Conv2D layer with n_classes filter, kernel size of 1 and a 'same' padding
+
+    conv10 = Conv2D(n_classes, 1, padding='same')(conv9)
+
+    model = tf.keras.Model(inputs=inputs, outputs=conv10)
+
+    return model
+
+# Set Model Dimensions
+img_height = 96
+img_width = 128
+num_channels = 3
+
+unet = unet_model((img_height, img_width, num_channels))
+
+# check ot the Model Summary
+unet.summary()
+
+# Model Compliling : Loss Function = SparseCategoricalCrossentropy
+unet.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# vizuaizing teh Results
+def display(display_list):
+    plt.figure(figsize=(15, 15))
+
+    title = ['Input Image', 'True Mask', 'Predicted Mask']
+
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i+1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+        plt.axis('off')
+    plt.show()
+
+# CHecking out some sample images
+for image, mask in image_ds.take(1):
+    sample_image, sample_mask = image, mask
+    print(mask.shape)
+display([sample_image, sample_mask])
+
+for image, mask in processed_image_ds.take(1):
+    sample_image, sample_mask = image, mask
+    print(mask.shape)
+display([sample_image, sample_mask])
+
+# Training teh Model on the CARLA self-driving car dataset
+EPOCHS = 40
+VAL_SUBSPLITS = 5
+BUFFER_SIZE = 500
+BATCH_SIZE = 32
+processed_image_ds.batch(BATCH_SIZE)
+train_dataset = processed_image_ds.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+print(processed_image_ds.element_spec)
+model_history = unet.fit(train_dataset, epochs=EPOCHS)
+
+# Create Predicted Masks
+def create_mask(pred_mask):
+    pred_mask = tf.argmax(pred_mask, axis=-1)
+    pred_mask = pred_mask[..., tf.newaxis]
+    return pred_mask[0]
+
+# Plot the model and show predictions
+plt.plot(model_history.history["accuracy"])
+
+def show_predictions(dataset=None, num=1):
+    """
+    Displays the first image of each of the num batches
+    """
+    if dataset:
+        for image, mask in dataset.take(num):
+            pred_mask = unet.predict(image)
+            display([image[0], mask[0], create_mask(pred_mask)])
+    else:
+        display([sample_image, sample_mask,
+             create_mask(unet.predict(sample_image[tf.newaxis, ...]))])
+
+show_predictions(train_dataset, 6)
+
+# END
